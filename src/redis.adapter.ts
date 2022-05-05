1a66ca4ca22d6d942f01bd1480164946ff9b66ec
chanminjo <happyjarban@gmail.com>
2022-05-05 15:08:32 +0900

    feat: redis downgrade version

--- src/redis.adapter.ts
+++ src/redis.adapter.ts
@@ -1,26 +1,26 @@
 import { IoAdapter } from '@nestjs/platform-socket.io';
 import { createAdapter } from '@socket.io/redis-adapter';
-import { createClient } from 'redis';
-import { ServerOptions } from 'ws';
-import { LoggerService } from './modules/logger/logger.service';
+import { RedisClient } from 'redis';
+import { Logger } from '@nestjs/common';
+import 'dotenv/config';
+
+const HOST: string = process.env.DB_REDIS_HOST ?? 'localhost';
+const PORT: number = process.env.DB_REDIS_PORT
+  ? parseInt(process.env.DB_REDIS_PORT)
+  : 6379;
+export const pubClient = new RedisClient({
+  host: HOST,
+  port: PORT,
+});
+export const subClient = pubClient.duplicate();
+
+const redisAdapter = createAdapter(pubClient, subClient);
 
 export class RedisIoAdapter extends IoAdapter {
-  private adapterConstructor: ReturnType<typeof createAdapter>;
-  private readonly logger = new LoggerService(RedisIoAdapter.name);
-
-  async connectToRedis(): Promise<void> {
-    const pubClient = createClient({ url: `redis://localhost:6379` });
-    const subClient = pubClient.duplicate();
-
-    await Promise.all([pubClient.connect(), subClient.connect()]);
-
-    this.adapterConstructor = createAdapter(pubClient, subClient);
-  }
-
-  createIOServer(port: number, options?: ServerOptions): any {
-    const server = super.createIOServer(port, options);
-    server.adapter(this.adapterConstructor);
-    this.logger.log(`Redis Adapted completed`);
+  createIOServer(port: number, options?: any): any {
+    const server: any = super.createIOServer(port, options);
+    server.adapter(redisAdapter);
+    Logger.log(`Redis Adapted ${process.env.DB_REDIS_HOST}:${PORT}`);
     return server;
   }
 }
